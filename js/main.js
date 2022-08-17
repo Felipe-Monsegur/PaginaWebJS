aafdfc64a0937b419098e60ad11ab2cf5f9b156b
Felipe Monsegur <felipemonsegur@gmail.com>
2022-08-17 12:46:35 -0300

    1.0.3

---
+++ js/main.js
@@ -0,0 +1,356 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Variables
+
+    //let baseDeDatos = [];
+
+    let carrito = [];
+    const DOMitems = document.getElementById('items');
+    const DOMcarrito = document.getElementById('carrito');
+    const DOMtotal = document.getElementById('total');
+    const DOMbotonVaciar = document.getElementById('boton-vaciar');
+    const DOMinputBuscador = document.getElementById('buscar-pal');
+    const DOMbotonCompra = document.getElementById('boton-compra');
+    const DOMmostrarCarrito = document.getElementById('mostrar-carrito');
+
+    const arayproductos = [{
+            nombre: "Iphone 13",
+            img: "img/Productos celulares/IPHONE 13.jpg",
+            alt: "iphone",
+            moneda: 400,
+            stock: 10,
+            id: 1,
+        },
+        {
+            nombre: "Iphone 13 mini",
+            img: "img/Productos celulares/IPHONE 13 MINI.jpg",
+            alt: "iphone",
+            moneda: 250,
+            stock: 15,
+            id: 2,
+        },
+        {
+            nombre: "Iphone 13 Pro max",
+            img: "img/Productos celulares/IPHONE 13 PRO MAX.jpg",
+            alt: "Iphone",
+            moneda: 1400,
+            stock: 11,
+            id: 3,
+        },
+        {
+            nombre: "Iphone 12",
+            img: "img/Productos celulares/IPHONE 12.jpg",
+            alt: "Iphone",
+            moneda: 470,
+            stock: 6,
+            id: 4,
+        },
+
+        {
+            nombre: "Iphone 11",
+            img: "img/Productos celulares/IPHONE 11.jpg",
+            alt: "Iphone",
+            moneda: 300,
+            stock: 1,
+            id: 5,
+        },
+
+        {
+            nombre: "MACBOOK AIR 13",
+            img: "img/Productos Laptops/macbook air 13.jpg",
+            alt: "macbook",
+            moneda: 500,
+            stock: 12,
+            id: 6,
+        },
+
+        {
+            nombre: "MACBOOK PRO 13",
+            img: "img/Productos Laptops/mcbook pro 13.jpg",
+            alt: "macbook",
+            moneda: 250,
+            stock: 23,
+            id: 7,
+        },
+
+        {
+            nombre: "MAC AIR CUSTOM",
+            img: "img/Productos Laptops/mac air custom.jpg",
+            alt: "macbook",
+            moneda: 250,
+            stock: 15,
+            id: 8,
+        },
+
+        {
+            nombre: "LAPTOP HP",
+            img: "img/Productos Laptops/laptop hp.jpg",
+            alt: "hp",
+            moneda: 250,
+            stock: 3,
+            id: 9,
+        },
+
+        {
+            nombre: "LAPTOP LENOVO",
+            img: "img/Productos Laptops/laptop lenovo.jpg",
+            alt: "lenovo",
+            moneda: 320,
+            stock: 11,
+            id: 10,
+        },
+
+        {
+            nombre: "LAPTOP DELL",
+            img: "img/Productos Laptops/laptop dell.jpg",
+            alt: "dell",
+            moneda: 320,
+            stock: 11,
+            id: 10,
+        },
+
+    ];
+
+    /**
+     * Dibuja todos los productos a partir de la base de datos. No confundir con el carrito
+     */
+    function renderizarProductos() {
+        DOMitems.innerHTML = '';
+        arayproductos.forEach((info) => {
+            // Estructura
+            const miNodo = document.createElement('div');
+            miNodo.classList.add('card', 'col-sm-4', );
+            // Body
+            const miNodoCardBody = document.createElement('div');
+            miNodoCardBody.classList.add('card-body');
+            // Titulo
+            const miNodoTitle = document.createElement('h3');
+            miNodoTitle.classList.add('card-title');
+            miNodoTitle.innerText = info.nombre;
+            // Precio
+            const miNodoPrecio = document.createElement('h5');
+            miNodoPrecio.classList.add('card-text');
+            miNodoPrecio.innerText = `$${info.moneda}`;
+            //Stock
+            const miNodoStock = document.createElement('h5');
+            miNodoStock.classList.add('card-text');
+            miNodoStock.innerText = `Stock: ${info.stock}`;
+            //Imagen
+            const miNodoImagen = document.createElement('img');
+            miNodoImagen.classList.add('image');
+            miNodoImagen.setAttribute('src', info.img);
+            // Boton 
+            const miNodoBoton = document.createElement('button');
+            miNodoBoton.classList.add('btn', 'btn-primary');
+            miNodoBoton.innerText = '+';
+            miNodoBoton.setAttribute('marcador', info.id);
+            miNodoBoton.addEventListener('click', anyadirProductoAlCarrito);
+            // Insertamos
+            miNodoCardBody.appendChild(miNodoImagen);
+            miNodoCardBody.append(miNodoTitle);
+            miNodoCardBody.append(miNodoPrecio);
+            miNodoCardBody.append(miNodoStock);
+            //miNodoCardBody.append(miNodoImagen)
+            miNodoCardBody.append(miNodoBoton);
+            miNodo.append(miNodoCardBody);
+            DOMitems.append(miNodo);
+        });
+    }
+
+    /**
+     * Evento para añadir un producto al carrito de la compra
+     */
+    function anyadirProductoAlCarrito(e) {
+        // Anyadimos el Nodo a nuestro carrito
+        carrito.push(e.target.getAttribute('marcador'))
+        arayproductos[e.target.getAttribute('marcador') - 1].stock--;
+        renderizarProductos();
+        // Actualizamos el carrito 
+        renderizarCarrito();
+        // Actualizamos el LocalStorage
+        guardarCarritoEnLocalStorage();
+        $.jGrowl("¡Agregado al carrito!", {
+            life: 1000
+        });
+    }
+    /**
+     * Dibuja todos los productos guardados en el carrito
+     */
+    function renderizarCarrito() {
+        // Vaciamos todo el html
+        DOMcarrito.innerText = '';
+        // Quitamos los duplicados
+        const carritoSinDuplicados = [...new Set(carrito)];
+        // Generamos los Nodos a partir de carrito
+        carritoSinDuplicados.forEach((item) => {
+            // Obtenemos el item que necesitamos de la variable base de datos
+            const miItem = arayproductos.filter((itemBaseDatos) => {
+                // ¿Coincide las id? Solo puede existir un caso
+                return itemBaseDatos.id === parseInt(item);
+            });
+            // Cuenta el número de veces que se repite el producto
+            const numeroUnidadesItem = carrito.reduce((total, itemId) => {
+                // ¿Coincide las id? Incremento el contador, en caso contrario no mantengo
+                return itemId === item ? total += 1 : total;
+            }, 0);
+            // Creamos el nodo del item del carrito
+            const miNodo = document.createElement('li');
+            miNodo.classList.add('list-group-item', 'text-right', 'mx-2');
+            miNodo.innerText = `${numeroUnidadesItem} x ${miItem[0].nombre} - $${miItem[0].moneda}`;
+            // Boton de borrar
+            const miBoton = document.createElement('button');
+            miBoton.classList.add('btn', 'btn-danger', 'mx-5');
+            miBoton.innerText = 'X';
+            miBoton.style.marginLeft = '1rem';
+            miBoton.dataset.item = item;
+            miBoton.addEventListener('click', borrarItemCarrito);
+            // Mezclamos nodos
+            miNodo.appendChild(miBoton);
+            DOMcarrito.appendChild(miNodo);
+        });
+        // Renderizamos el precio total en el HTML
+        DOMtotal.innerText = calcularTotal();
+    }
+
+    /**
+     * Evento para borrar un elemento del carrito
+     */
+    function borrarItemCarrito(e) {
+        // Obtenemos el producto ID que hay en el boton pulsado
+        const id = e.target.dataset.item;
+        // Borramos todos los productos
+        carrito = carrito.filter((carritoId) => {
+            return carritoId !== id;
+        });
+        // volvemos a renderizar
+        renderizarCarrito();
+        // Actualizamos el LocalStorage
+        guardarCarritoEnLocalStorage();
+        $.jGrowl("¡Elemento eliminado!", {
+            life: 1000,
+            theme: 'test'
+        });
+
+    }
+
+    /**
+     * Calcula el precio total teniendo en cuenta los productos repetidos
+     */
+    function calcularTotal() {
+        // Recorremos el array del carrito 
+        return carrito.reduce((total, item) => {
+            // De cada elemento obtenemos su precio
+            const miItem = arayproductos.filter((itemBaseDatos) => {
+                return itemBaseDatos.id === parseInt(item);
+            });
+            // Los sumamos al total
+            return total + miItem[0].moneda;
+        }, 0).toFixed(2);
+    }
+
+    /**
+     * Varia el carrito y vuelve a dibujarlo
+     */
+    function vaciarCarrito() {
+        // Limpiamos los productos guardados
+        carrito = [];
+        // Renderizamos los cambios
+        renderizarCarrito();
+        // Borra LocalStorage
+        localStorage.removeItem('carrito');
+        $.jGrowl("¡Carrito vaciado!", {
+            life: 1000,
+            theme: 'test'
+        });
+
+    }
+
+    function guardarCarritoEnLocalStorage() {
+        localStorage.setItem('carrito', JSON.stringify(carrito));
+    }
+
+    function cargarCarritoDeLocalStorage() {
+        // ¿Existe un carrito previo guardado en LocalStorage?
+        if (localStorage.getItem('carrito') !== null) {
+            // Carga la información
+            carrito = JSON.parse(localStorage.getItem('carrito'));
+        }
+    }
+
+    function filtrarProductos() {
+        const Productos = Array.from(document.querySelectorAll('#items .card .card-body .card-title'));
+        Productos.forEach(producto => {
+            let productoStyle = producto.parentElement.parentElement.style;
+            if (producto.innerText.toLowerCase().includes(DOMinputBuscador.value.toLowerCase()) || DOMinputBuscador.value == '') {
+                productoStyle.display = "initial";
+                setTimeout(() => {
+                    productoStyle.transform = 'scale(100%)'
+                }, 300)
+
+            } else {
+                productoStyle.transform = 'scale(0%)';
+                setTimeout(() => {
+                    productoStyle.display = 'none'
+                }, 300)
+            }
+        })
+    }
+
+    function alertaCompra() {
+        Swal.fire({
+            icon: 'question',
+            title: 'Deseas confirmar tu compra?',
+            showDenyButton: true,
+            showCancelButton: false,
+            confirmButtonText: 'Si',
+            denyButtonText: `Cancelar compra`,
+        }).then((result) => {
+            /* Read more about isConfirmed, isDenied below */
+            if (result.isConfirmed) {
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Gracias por tu compra,nos estaremos comunicando por email.',
+                    input: 'email',
+                    inputLabel: 'Ingrese su Email',
+                    inputPlaceholder: 'Ingrese su Email'
+                })
+            } else if (result.isDenied) {
+                Swal.fire('Su compra fue cancelada.', '', 'info');
+
+            }
+        })
+    }
+
+    function mostrarCarrito() {
+        let elemento = document.getElementById("carri");
+        let elem = document.getElementById("mostrar-carrito");
+        if (elemento.className == "col-sm-4 ocult") {
+            elemento.className = "col-sm-4 show";
+            elem.innerText = "Cerrar carrito";
+        } else {
+            elemento.className = "col-sm-4 ocult";
+            elem.innerText = "Carrito";
+        }
+    }
+
+
+    // Eventos
+    DOMbotonVaciar.addEventListener('click', vaciarCarrito);
+    DOMinputBuscador.addEventListener('keyup', filtrarProductos);
+    DOMbotonCompra.addEventListener('click', alertaCompra);
+    DOMmostrarCarrito.addEventListener('click', mostrarCarrito);
+
+
+
+
+
+
+
+
+    // Inicio
+    cargarCarritoDeLocalStorage();
+    renderizarProductos();
+    renderizarCarrito();
+
+
+});
